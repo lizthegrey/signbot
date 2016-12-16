e389c4ea689ee63a38707ecdebad234438b81b9a
Liz Fong-Jones <elizabeth@ugcs.caltech.edu>
2016-12-16 07:25:40 -0500

    add backend code

---
+++ backend/src/signprocessor/process.go
@@ -0,0 +1,184 @@
+package signprocessor
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"strings"
+	"time"
+
+	"github.com/google/go-github/github"
+	"github.com/mrjones/oauth"
+	"github.com/zabawaba99/fireauth"
+	"github.com/zabawaba99/firego"
+
+	"golang.org/x/oauth2"
+)
+
+func EventLoop() {
+	data := make(fireauth.Data)
+	options := fireauth.Option{
+		Admin: true,
+	}
+	token, _ := fireauth.New(Secrets.FireBaseSecret).CreateToken(data, &options)
+	f := firego.New(Config.FireBaseDB, nil)
+	f.Auth(token)
+
+	t := oauth.NewConsumer(Secrets.TwitterKey, Secrets.TwitterSecret, oauth.ServiceProvider{
+		RequestTokenUrl:   "https://api.twitter.com/oauth/request_token",
+		AuthorizeTokenUrl: "https://api.twitter.com/oauth/authorize",
+		AccessTokenUrl:    "https://api.twitter.com/oauth/access_token",
+	})
+
+	ts := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: Secrets.GithubToken},
+	)
+	tc := oauth2.NewClient(oauth2.NoContext, ts)
+	gh := github.NewClient(tc)
+
+	notifications := make(chan firego.Event)
+	if err := f.Watch(notifications); err != nil {
+		log.Fatalf("Error setting up watch: %v", err)
+	}
+
+	defer f.StopWatching()
+	for event := range notifications {
+		if event.Path == "/" && event.Data != nil {
+			if users, ok := event.Data.(map[string]interface{})["users"]; ok {
+				for uid, d := range users.(map[string]interface{}) {
+					details := d.(map[string]interface{})
+					if process(uid, details, t, gh) {
+						f.Child("users").Child(uid).Remove()
+					}
+				}
+			}
+		} else if event.Type == "put" && strings.HasPrefix(event.Path, "/users/") {
+			uid := strings.TrimPrefix(event.Path, "/users/")
+			details := event.Data.(map[string]interface{})
+			if process(uid, details, t, gh) {
+				f.Child("users").Child(uid).Remove()
+			}
+		}
+	}
+	fmt.Printf("Notifications have stopped\n")
+}
+
+func process(uid string, details map[string]interface{}, t *oauth.Consumer, gh *github.Client) bool {
+	linkProfile := details["linkProfile"].(bool)
+	name := details["name"].(string)
+	title := details["title"].(string)
+	affiliation := details["affiliation"].(string)
+
+	secret := details["twitterSecret"].(string)
+	token := details["twitterToken"].(string)
+	tclient, err := t.MakeHttpClient(&oauth.AccessToken{Token: token, Secret: secret})
+	resp, err := tclient.Get("https://api.twitter.com/1.1/account/verify_credentials.json?skip_status=true&include_entities=false")
+	if err != nil {
+		log.Printf("Twitter API error: %v processing %s", err, uid)
+		return false
+	}
+	defer resp.Body.Close()
+
+	bits, _ := ioutil.ReadAll(resp.Body)
+
+	var user map[string]interface{}
+	json.Unmarshal(bits, &user)
+	egg := user["default_profile_image"].(bool)
+	created, _ := time.Parse(time.RubyDate, user["created_at"].(string))
+	description := user["description"].(string)
+	followers := int(user["followers_count"].(float64))
+	following := int(user["friends_count"].(float64))
+	displayName := user["name"].(string)
+	handle := user["screen_name"].(string)
+	tweets := int(user["statuses_count"].(float64))
+	var url string
+	switch user["url"].(type) {
+	case string:
+		url = user["url"].(string)
+	}
+
+	if name == "" {
+		log.Printf("No signatory name specified for %s (%s)", uid, handle)
+		return false
+	}
+
+	body := fmt.Sprintf("Twitter user: https://twitter.com/%s\nDisplay name: %s\nURL: %s\nCreated: %v, Followers: %d, Following: %d, Tweets: %d, Egg: %v\nTagline: %s", handle, displayName, url, created, followers, following, tweets, egg, description)
+
+	var linkMd, affiliationMd, titleMd string
+	if linkProfile {
+		linkMd = fmt.Sprintf("  link: https://twitter.com/%s\n", handle)
+	}
+	if affiliation != "" {
+		affiliationMd = fmt.Sprintf("  affiliation: \"%s\"\n", affiliation)
+	}
+	if title != "" {
+		titleMd = fmt.Sprintf("  occupation_title: \"%s\"\n", title)
+	}
+	contents := fmt.Sprintf("---\n  name: \"%s\"\n%s%s%s---", name, linkMd, affiliationMd, titleMd)
+
+	// Ensure we are forking from a clean state.
+	g := gh.Git
+	ref, _, err := g.GetRef("neveragaindottech", "neveragaindottech.github.io", "heads/master")
+	if err != nil {
+		log.Printf("Error: %v processing %s", err, uid)
+		return false
+	}
+	refStr := fmt.Sprintf("heads/%s", uid)
+	ref.Ref = &refStr
+	if _, _, err = g.UpdateRef(Config.GithubUser, "neveragaindottech.github.io", ref, true); err != nil {
+		if _, _, err = g.CreateRef(Config.GithubUser, "neveragaindottech.github.io", ref); err != nil {
+			log.Printf("Error: %v processing %s", err, uid)
+			return false
+		}
+	}
+	baseC, _, err := g.GetCommit(Config.GithubUser, "neveragaindottech.github.io", *ref.Object.SHA)
+	if err != nil {
+		log.Printf("Error: %v processing %s", err, uid)
+		return false
+	}
+	path := fmt.Sprintf("_signatures/%s.md", uid)
+	mode := "100644"
+	kind := "blob"
+	newT, _, err := g.CreateTree(Config.GithubUser, "neveragaindottech.github.io", *baseC.Tree.SHA, []github.TreeEntry{
+		{Path: &path, Mode: &mode, Type: &kind, Content: &contents},
+	})
+	if err != nil {
+		log.Printf("Error: %v processing %s", err, uid)
+		return false
+	}
+	desc := fmt.Sprintf("SignBot: Add signatory '%s' (%s)", name, handle)
+	newC, _, err := g.CreateCommit(Config.GithubUser, "neveragaindottech.github.io", &github.Commit{
+		Message: &desc,
+		Tree:    newT,
+		Parents: []github.Commit{*baseC},
+	})
+	if err != nil {
+		log.Printf("Error: %v processing %s", err, uid)
+		return false
+	}
+	if _, _, err = g.UpdateRef(Config.GithubUser, "neveragaindottech.github.io", &github.Reference{
+		Ref: &refStr,
+		Object: &github.GitObject{
+			SHA: newC.SHA,
+		},
+	}, false); err != nil {
+		log.Printf("Error: %v processing %s", err, uid)
+		return false
+	}
+	p := gh.PullRequests
+	branchName := fmt.Sprintf("%s:%s", Config.GithubUser, uid)
+	master := "master"
+	_, _, err = p.Create("neveragaindottech", "neveragaindottech.github.io", &github.NewPullRequest{
+		Title: &desc,
+		Head:  &branchName,
+		Base:  &master,
+		Body:  &body,
+	})
+	if err != nil {
+		log.Printf("Error: %v processing %s", err, uid)
+		return false
+	}
+	fmt.Printf("Processed %s (https://twitter.com/%s)\n", uid, handle)
+	return true
+}
---
+++ backend/src/signprocessor_main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"signprocessor"
+)
+
+func main() {
+	signprocessor.EventLoop()
+}
